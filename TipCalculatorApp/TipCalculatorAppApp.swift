2ef8f18c19a04a0c9d864f38b73117e693a47d26
Tori Luce <victorialuce34@gmail.com>
2025-02-02 18:09:16 -0600

    Initial Commit

---
+++ TipCalculatorApp/TipCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorAppApp.swift
+//  TipCalculatorApp
+//
+//  Created by Victoria Luce on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TipCalculatorAppTests/TipCalculatorAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TipCalculatorAppTests.swift
+//  TipCalculatorAppTests
+//
+//  Created by Victoria Luce on 2/2/25.
+//
+
+import Testing
+
+struct TipCalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
